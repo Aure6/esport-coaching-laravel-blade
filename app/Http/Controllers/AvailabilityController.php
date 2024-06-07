945267a056109f7ddacfba8c168ecb91c3be5801
Aure6 <aurelien7112001@gmail.com>
2024-06-07 12:25:50 +0200

    Removed unnecessary comment in AvailabilityController's update method

--- app/Http/Controllers/AvailabilityController.php
+++ app/Http/Controllers/AvailabilityController.php
@@ -9,20 +9,18 @@ class AvailabilityController extends Controller
 {
     public function update(Request $request)
     {
-        $user = auth()->user(); // Get the currently authenticated user
+        $user = auth()->user();
 
         foreach (['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'] as $day) {
             if ($request->has(strtolower($day) . '_checkbox')) {
                 $startTime = $request->input(strtolower($day) . '_start') . ':00';
                 $endTime = $request->input(strtolower($day) . '_end') . ':00';
 
-                // Update the availability if it exists, otherwise create a new one
                 Availability::updateOrCreate(
                     ['coach_id' => $user->id, 'day_of_week' => $day],
                     ['start_time' => $startTime, 'end_time' => $endTime]
                 );
             } else {
-                // Delete the availability if it exists
                 Availability::where('coach_id', $user->id)->where('day_of_week', $day)->delete();
             }
         }
